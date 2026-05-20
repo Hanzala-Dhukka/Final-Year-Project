0dbc355ce7c71411e1b89e14de8c4f38510efd86
Hanzala-Dhukka <hanzalahatimtai9@gmail.com>
2026-05-20 21:38:16 +0530

    feat: implement user registration and authentication boilerplate with password hashing

---
+++ cybershield/backend/app/models/user_model.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel, EmailStr
+from datetime import datetime
+
+class UserRegister(BaseModel):
+    name: str
+    email: EmailStr
+    password: str
+
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class UserResponse(BaseModel):
+    id: str
+    name: str
+    email: EmailStr
+    created_at: datetime
--- cybershield/backend/main.py
+++ cybershield/backend/main.py
@@ -1,11 +1,20 @@
 from fastapi import FastAPI
 from app.database.db import database
+from app.routes.auth_routes import router as auth_router
 
 app = FastAPI()
 
+app.include_router(
+    auth_router,
+    prefix="/api/auth",
+    tags=["Authentication"]
+)
+
 @app.get("/")
 def home():
-    return {"message": "CyberShield Backend Running"}
+    return {
+        "message": "CyberShield Backend Running"
+    }
 
 @app.get("/test-db")
 async def test_db():
